ed509387fba895700c4d116fb62bf2a7214c6995
Gavin Wen <349088304@qq.com>
2019-02-01 15:38:36 +0800

    submit

---
+++ index.js
@@ -0,0 +1,7 @@
+// import { name, age, sex } from './tools/tool.js';
+
+
+import Students from './tools/tool.js
+
+
+
---
+++ tools/tool.js
@@ -0,0 +1,26 @@
+export default {
+    data: {
+        lists: [
+            {
+                name: "张三",
+                sex: "男",
+                age: "18"
+            },
+            {
+                name: "李四",
+                sex: "男",
+                age: "18"
+            }
+        ]
+    }
+}
+
+// let name = "zhangsan";
+// let age = "18";
+// let sex = "girl";
+
+// export {
+//     name,
+//     age,
+//     sex
+// }
